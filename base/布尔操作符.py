96874866b2d4c3c680069c952ac7165c91cc95b1
3187440464 <3187440464@qq.com>
2025-05-12 03:55:44 +0000

    第一天完成任务

---
+++ base/布尔操作符.py
@@ -0,0 +1,14 @@
+#二元布尔操作符
+print(True and True)
+print(False or True)
+# and 操作符
+print(True and True)
+print(True and False)
+print(False and False)
+# or 操作符
+print(True or True)
+print(True or False)
+print(False or False)
+# not 操作符【一元操作符】
+print(not True)
+print(not False)
---
+++ base/控制流语句.py
@@ -0,0 +1,10 @@
+# if else elif 语句
+name='Bob'
+if name == 'Alice':
+    print('Hi,Alice')
+elif name == 'Bob':
+    print('Your name is Bob')
+else:
+    print('name no valide')
+
+# while 循环
