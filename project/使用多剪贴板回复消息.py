3882a0f8e00e5352c406478fe54269844fae3796
px_computer <3187440464@qq.com>
2025-07-02 21:06:49 +0800

    - refactor：

--- project/使用多剪贴板回复消息.py
+++ project/使用多剪贴板回复消息.py
@@ -1,8 +1,8 @@
 #这是本项目正儿八经的第一个项目，使用多剪切板达到回复消息的作用
+import sys,pyperclip
 TEXT={'agree':'''我同意，我过会去找你''',
       'busy':'''不好意思，我这几天很忙''',
       'upsell':'''我会考虑一下你的提议'''}
-import sys,pyperclip
 if len(sys.argv) < 2:
       print('使用方法：python filename [keyphrase] - 要加入命令参数')
       sys.exit()
